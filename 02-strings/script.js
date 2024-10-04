786154b70f33d5a0a750930f5a2c7c47c751e3db
IvanVitali89 <ertre89@gmail.com>
2024-10-04 17:37:15 +0200

    added second variable

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,10 +2,20 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
+const myName = 'Ivan'
+console.log(myName);
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
+const number = 8;
+const text = 'Ciao'
+const bool = true
+console.log(number);
+console.log(text);
+console.log(bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
